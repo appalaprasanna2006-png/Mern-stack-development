abff8d6f08e4291d40f88c0bc41b7ebaff2f3211
appalaprasanna2006 <appalaprasanna2006@gmail.com.com>
2026-08-27 10:14:30 +0530

    msg

---
+++ 00-Folder/new.js
@@ -0,0 +1,12 @@
+let b=20;
+let a=10;
+let arr=[1,2,3]
+function myn(a,b) {
+    return "myn"
+}
+let d=(e1,e2,e3)=>{
+    return e1*e2*e3;
+}
+export{myn};
+export{a,arr,d};
+export default b;
---
+++ 00-Folder/script.js
@@ -0,0 +1,9 @@
+// let a= document.createElement('h1')
+// a.innerHTML="thank you."
+// document.body.appendChild(a)/
+import b from './new.js';
+import {a,arr,d} from './new.js';
+import {myn} from './new.js';
+console.log(b);
+console.log(a,arr,d(1,2,3));
+console.log(myn())
