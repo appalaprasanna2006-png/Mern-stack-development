028d5b5648d9bef76208e04b331bfd232aedcae3
appalaprasanna2006 <appalaprasanna2006@gmail.com.com>
2026-08-31 11:57:06 +0530

    msg

---
+++ 06-folder/myapp/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500'>
+      <h1 className='text-blue-900'>This is book</h1>
+      
+    </div>
+  )
+}
+
+export default App
